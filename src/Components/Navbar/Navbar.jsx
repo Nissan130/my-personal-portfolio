801a6c851f48d4696da241be0a48a4549e6c32a2
nissan <mdnissanali130@gmail.com>
2025-07-12 17:44:04 +0600

    Initialize to git

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='navbarContainer'>
+        <div className='logo'>
+            <h3 >Nissan</h3>
+        </div>
+
+        <div className='navbarLinks'>
+            <a className='navLinkItem' href="#about">Home</a>
+            <a className='navLinkItem' href="#about">About</a>
+            <a className='navLinkItem' href="#project">Project</a>
+            <a className='navLinkItem' href="#contact">Contact</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
