65e2eb33ee1389bdae060526b874522bf92c504f
Nissan <mdnissanali130@gmail.com>
2025-08-30 02:17:39 +0600

    update(contact): add emailjs feature

--- src/Components/ContactSection/ContactSection.jsx
+++ src/Components/ContactSection/ContactSection.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ContactSection.css'
 import { MdEmail } from "react-icons/md";
 import { FaSquarePhone } from "react-icons/fa6";
 import { FaLocationArrow } from "react-icons/fa";
+import emailjs from "emailjs-com";
 
 function ContactSection() {
+
+  const [loading, setLoading] = useState(false);
+  //send message functionality
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+  const [status, setStatus] = useState("");
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+  const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true); // show spinner
+
+    emailjs
+      .send(
+        // process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        serviceID,
+        // process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        templateID,
+        formData,
+        // process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        publicKey
+
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setStatus("Message sent successfully!");
+          setFormData({ name: "", email: "", message: "" });
+          setLoading(false);
+        },
+        (error) => {
+          console.log(error.text);
+          setStatus("Failed to send message. Try again.");
+          setLoading(false);
+        }
+      );
+  };
+
+
   return (
     <div className='contactSection'>
       <div className="contactSectionHeading">
@@ -13,7 +63,7 @@ function ContactSection() {
       </div>
 
       <div className="contactContainer">
-        
+
         {/* Contact Info */}
         <div className="get_in_touch_container">
           <h4 className="get_in_touch_heading">Get In Touch</h4>
@@ -36,21 +86,43 @@ function ContactSection() {
         {/* Contact Form */}
         <div className="formContainer">
           <h4 className="formContainerHeading">Send a Message</h4>
-          <form className="contactForm">
+          <form className="contactForm" onSubmit={sendEmail}>
             <div className="inputGroup">
-              <input type="text" name="name" required />
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required />
               <label>Your Name</label>
             </div>
+
             <div className="inputGroup">
-              <input type="email" name="email" required />
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+
+              />
               <label>Your Email</label>
             </div>
             <div className="inputGroup">
-              <textarea name="message" rows="5" required></textarea>
+              <textarea
+                name="message"
+                rows="5"
+                value={formData.message}
+                onChange={handleChange}
+                required
+
+              ></textarea>
               <label>Your Message</label>
             </div>
-            <button type="submit" className="submitBtn">Send Message</button>
+            <button type="submit" className="submitBtn" disabled={loading}>{loading ? <span className="spinner"></span> : "Send Message"}
+            </button>
           </form>
+          {status && <p className="statusMessage">{status}</p>}
         </div>
 
       </div>
